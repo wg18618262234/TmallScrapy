d2ec70aba20f42462d798f606e711f5d1bec4bb3
wg18618262234 <https://github.com/wg18618262234/JDReptail.git>
2019-03-22 17:46:14 +0800

    url revise

--- Tmall/Tmall/spiders/goods.py
+++ Tmall/Tmall/spiders/goods.py
@@ -15,8 +15,7 @@ class GoodsSpider(scrapy.Spider):
     #     'USER_AGENT': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
     #     # 'Referer': 'http://list.tmall.com/search_product.htm?spm=a223c.7814651.navcategory176.1&cat=50025145&from=nav_ct_176_8141_8142',
     # }
-    start_urls = [
-        'https://ochirly.m.tmall.com/shop/shop_auction_search.do?p=1&page_size=12&shop_id=58674200']
+    start_urls = ['https://ochirly.m.tmall.com/shop/shop_auction_search.do?p=1&page_size=12']
 
     def parse(self, response):
         open('goodsinfo.txt', 'wb').write(response.text.encode('utf8'))
@@ -30,10 +29,8 @@ class GoodsSpider(scrapy.Spider):
         items = data['items']
         for p in range(1, int(total_page) + 1):
             print(p)
-            yield scrapy.Request(
-                url='https://ochirly.m.tmall.com/shop/shop_auction_search.do?page_size=12&shop_id=58674200&p=' + str(
-                    p),
-                callback=self.parse2, dont_filter=True)
+            yield scrapy.Request(url='https://ochirly.m.tmall.com/shop/shop_auction_search.do?page_size=12&p=' + str(p),
+                                 callback=self.parse2, dont_filter=True)
 
     def parse2(self, response):
         print(response.url)
